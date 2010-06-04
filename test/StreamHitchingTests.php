564e6936435862f4db080094ed786b0a18bda415
caefer <caefer@ical.ly>
2010-06-04 10:06:44 +0200

    added remote test to suite

--- test/StreamHitchingTests.php
+++ test/StreamHitchingTests.php
@@ -6,6 +6,7 @@ require_once dirname(__FILE__).'/Stream/Wrapper/DecoratorTest.php';
 require_once dirname(__FILE__).'/Stream/Wrapper/Decorator/AnalizerTest.php';
 require_once dirname(__FILE__).'/Stream/Wrapper/ReadOnlyFile/LocalTest.php';
 require_once dirname(__FILE__).'/Stream/Wrapper/ReadOnlyFile/MockTest.php';
+require_once dirname(__FILE__).'/Stream/Wrapper/ReadOnlyFile/RemoteTest.php';
 
 class StreamHitchingTests
 {
@@ -17,6 +18,7 @@ class StreamHitchingTests
     $suite->addTestSuite('Stream_Wrapper_Decorator_Analyzer_Test');
     $suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Local_Test');
     $suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Mock_Test');
+    $suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Remote_Test');
     return $suite;
   }
 }
