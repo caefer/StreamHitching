9664a4fe55c55c6caa258775db042eb3b2031c1e
caefer <caefer@ical.ly>
2010-06-08 10:57:37 +0200

    replaced requires with bootstrap and added some more test cases to the execution

--- test/StreamHitchingTests.php
+++ test/StreamHitchingTests.php
@@ -1,12 +1,9 @@
 <?php
 
+/** PHPUnit Framework */
 require_once 'PHPUnit/Framework.php';
-require_once dirname(__FILE__).'/Stream/SourceFilter/MockTest.php';
-require_once dirname(__FILE__).'/Stream/Wrapper/DecoratorTest.php';
-require_once dirname(__FILE__).'/Stream/Wrapper/Decorator/AnalizerTest.php';
-require_once dirname(__FILE__).'/Stream/Wrapper/ReadOnlyFile/LocalTest.php';
-require_once dirname(__FILE__).'/Stream/Wrapper/ReadOnlyFile/MockTest.php';
-require_once dirname(__FILE__).'/Stream/Wrapper/ReadOnlyFile/RemoteTest.php';
+/** Autoloading classes if necessary */
+require_once dirname(__FILE__).'/bootstrap.php';
 
 class StreamHitchingTests
 {
@@ -16,9 +13,11 @@ class StreamHitchingTests
     $suite->addTestSuite('Stream_SourceFilter_Mock_Test');
     $suite->addTestSuite('Stream_Wrapper_Decorator_Test');
     $suite->addTestSuite('Stream_Wrapper_Decorator_Analyzer_Test');
-    $suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Local_Test');
-    $suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Mock_Test');
-    $suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Remote_Test');
+    $suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_PHPLocal_Test');
+    $suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_HTTP_Test');
+    #$suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Local_Test');
+    #$suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Mock_Test');
+    #$suite->addTestSuite('Stream_Wrapper_ReadOnlyFile_Remote_Test');
     return $suite;
   }
 }
